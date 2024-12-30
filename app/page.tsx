978076428a264d80a5b3035a8e1853c15a0545ae
Misty-clouds <adenijitaoheed26@gmail.com>
2024-12-30 10:04:18 +0100

    Next steps removed

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,4 @@
 import Hero from "@/components/hero";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
 import Description from "@/components/Description";
 
 export default async function Home() {
@@ -8,9 +7,7 @@ export default async function Home() {
       <Hero />
       <Description/>
       <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        <SignUpUserSteps /> 
-        
+        <h2 className="font-medium text-xl mb-4">Next steps</h2>        
       </main>
     </>
   );
